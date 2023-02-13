acb4d0844c37dd8b6114a647170b8708f51b4acc
kaiuri <19148108+kaiuri@users.noreply.github.com>
2023-02-13 06:42:52 -0300

    chore: format

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-import * as Path   from "https://deno.land/std@0.177.0/path/mod.ts";
+import * as Path from "https://deno.land/std@0.177.0/path/mod.ts";
 import * as Assert from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import * as Async  from "https://deno.land/std@0.177.0/async/mod.ts";
+import * as Async from "https://deno.land/std@0.177.0/async/mod.ts";
 
 export { Assert, Async, Path };
